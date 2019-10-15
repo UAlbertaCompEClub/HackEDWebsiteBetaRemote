ba73112d148c790ba957ff6f961ad94936ebb809
Kevin de Haan <kbehaan@gmail.com>
2019-10-15 10:06:42 -0600

    Updates

--- precache-manifest.86fb1773af458494b09ff655f599399b.js
+++ precache-manifest.86fb1773af458494b09ff655f599399b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cea1ec478ce4dce89cf6c19fd29251ed",
+    "revision": "782c6175115b43ded1712e8c4bbb931d",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.621b5bde.chunk.css"
   },
   {
-    "revision": "edfdb6b0d247692de434",
-    "url": "/static/css/main.a850100b.chunk.css"
+    "revision": "aa3ac1c5dbefffb4d11c",
+    "url": "/static/css/main.e9cd1ced.chunk.css"
   },
   {
     "revision": "8607ca436c225a2eaf46",
     "url": "/static/js/2.1b8d238c.chunk.js"
   },
   {
-    "revision": "edfdb6b0d247692de434",
-    "url": "/static/js/main.23b60408.chunk.js"
+    "revision": "aa3ac1c5dbefffb4d11c",
+    "url": "/static/js/main.65ed0540.chunk.js"
   },
   {
     "revision": "5a8c2f52c13e5da62a7a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.266d6f13ffb8d12d71a0ac7396ce4bbd.js"
+  "/precache-manifest.86fb1773af458494b09ff655f599399b.js"
 );
 
 self.addEventListener('message', (event) => {
